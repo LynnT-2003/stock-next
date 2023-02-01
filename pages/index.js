5fdfc6f19af99aea65f408acea393486399a43e1
LynnT-2003 <lnyinyi22@gmail.com>
2023-02-01 15:13:05 +0700

    Added my name

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
+            Lynn Thit Nyi Nyi
           </p>
           <div>
             <a
